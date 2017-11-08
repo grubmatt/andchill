60955ad0a790b7d66bd8b4949bf60853b4d39bce
Matt <mattgruber61@gmail.com>
2017-11-07 20:17:13 -0500

    Add webview whitelisting

--- message_processesing.js
+++ message_processesing.js
@@ -41,7 +41,11 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": PAGE_ACCESS_TOKEN,
+            "whitelisted_domains":[
+              "https://xandchill.herokuapp.com"
+            ]
+          },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
