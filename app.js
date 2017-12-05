7c8dc065e5b982f1c7e56c77f6e499c82f9df7aa
Matt <mattgruber61@gmail.com>
2017-12-04 22:04:57 -0500

    Reorganize routes

--- app.js
+++ app.js
@@ -14,6 +14,32 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// Index
+app.get('/', (req, res) => {
+  res.render('location')
+});
+
+// Webhook Routes
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
 app.post('/webhook', (req, res) => {  
   let body = req.body;
   if (body.object === 'page') {
@@ -40,48 +66,25 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.render('location')
-});
-
-app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN
-  // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-  // Check if a token and mode were sent
-  if (mode && token) {
-    // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
+// Plan Routes
+app.get('/plan/:planId', (req, res) => {
+  var planId = req.params.planId
+  var rests = Event.find(planId, (rests)=> {
+    console.log(rests)
+    res.render('yelp', {rests: rests})
+  })
 });
 
 app.put('/new/plan/:ownerId/:lat/:lng', (req,res) => {
   Plan.create(req.params.ownerId, req.params.lat, req.params.lng, (plan) => {
     res.send(plan)
   });
-})
-
-app.get('/plan/:planId', (req, res) => {
-  var planId = req.params.planId
-  var rests = Event.find(planId, (rests)=> {
-      console.log(rests)
-      res.render('yelp', {rests: rests})
-  })
 });
 
 app.get('/restaurants/:planId', (req, res) => {
   var planId = req.params.planId
   var rests = Event.find(planId, (rests)=> {
-      console.log(rests)
-      res.render('yelp', {rests: rests})
-  })
-})
+    console.log(rests)
+    res.render('yelp', {rests: rests})
+  });
+});
